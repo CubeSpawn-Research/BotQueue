829a4721a26cdeebfe9b22d7fe6444f07ee09647
Justin Nesselrotte <jnesselr@harding.edu>
2015-08-29 02:01:33 -0600

    Removed old generic field file

--- resources/views/fields/generic.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="control-group">
-	{!! $labelField !!}
-	<div class="controls">
-		<input type="{{ $type }}" class="input-xlarge" name="{{$name}}" value="{{$value}}">
-	</div>
-	{!! $errorText !!}
-	{!! $helpText !!}
-</div>
