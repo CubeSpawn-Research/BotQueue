65db70835415e1e7ecc7606d227ed163462cecaf
Justin Nesselrotte <jnesselr@harding.edu>
2015-08-29 16:12:21 -0600

    Cleaning up the routes a bit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,44 +13,33 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('home', 'HomeController@index');
+Route::get('about', 'InfoController@about');
 
 // Registration and login
 Route::get('register', 'AuthController@getLoginAndRegister');
 Route::post('register', 'AuthController@postRegister');
+
 Route::get('login', 'AuthController@getLoginAndRegister');
 Route::post('login', 'AuthController@postLogin');
+
 Route::get('logout', 'AuthController@logout');
 
-Route::group([
-    'as' => 'bot:',
-    'namespace' => 'Bot',
-    'prefix' => 'bot'
-], function () {
-    Route::get('register', ['as' => 'register', 'uses' => 'EditController@getRegister']);
-    Route::post('register', 'EditController@postRegister');
-});
+// Bot pages
+Route::get('bot/register', ['as' => 'bot:register', 'uses' => 'Bot\EditController@getRegister']);
+Route::post('bot/register', 'Bot\EditController@postRegister');
 
-Route::group([
-    'as' => 'bot:',
-    'namespace' => 'Bot',
-    'prefix' => 'bot/{bot}'
-], function () {
-    Route::group([
-        'as' => 'edit:',
-        'prefix' => 'edit'
-    ], function () {
-        Route::get('queues', ['as' => 'queues', 'uses' => 'EditController@getQueues']);
-        Route::post('queues', 'EditController@postQueues');
-    });
-});
+Route::get('bot/{bot}/edit/queues', ['as' => 'bot:edit:queues', 'uses' => 'Bot\EditController@getQueues']);
+Route::post('bot/{bot}/edit/queues', 'Bot\EditController@postQueues');
 
+// Queue pages
 Route::get('queues', 'QueueController@index');
+Route::get('queue/{queue}', ['as' => 'queue', 'uses' => 'QueueController@view']);
+
 Route::get('queue/create', ['as' => 'queue:create', 'uses' => 'QueueController@getCreate']);
 Route::post('queue/create', 'QueueController@postCreate');
-Route::get('queue/{queue}', ['as' => 'queue', 'uses' => 'QueueController@view']);
+
 Route::get('queue/{queue}/edit', ['as' => 'queue:edit', 'uses' => 'QueueController@getEdit']);
 Route::post('queue/{queue}/edit', 'QueueController@postEdit');
+
 Route::get('queue/{queue}/delete', ['as' => 'queue:delete', 'uses' => 'QueueController@getDelete']);
 Route::post('queue/{queue}/delete', 'QueueController@postDelete');
-
-Route::get('about', 'InfoController@about');
