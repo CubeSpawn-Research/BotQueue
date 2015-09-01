99e55e327ff4f26698df0d13157768501b4ea6be
Justin Nesselrotte <jnesselr@harding.edu>
2015-09-01 08:29:02 -0600

    Upgraded again due to laravel/framework#10117

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,15 +1,16 @@
 <?php namespace App\Models;
 
-use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Support\Facades\Hash;
 
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-
-class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+class User extends Model implements AuthorizableContract,
+    AuthenticatableContract, CanResetPasswordContract {
 
 	use Authorizable, Authenticatable, CanResetPassword;
 
