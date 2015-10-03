0cb22d4cf2d19f8e070d1c428b710fd736f6e1eb
Justin Nesselrotte <jnesselr@harding.edu>
2015-10-02 20:03:17 -0600

    Cleaning and fixing AddMaxFileSizeError middleware

--- app/Http/Middleware/AddMaxFileSizeError.php
+++ app/Http/Middleware/AddMaxFileSizeError.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\MessageBag;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AddMaxFileSizeError
 {
@@ -20,8 +21,7 @@ class AddMaxFileSizeError
         $errors = new MessageBag();
         foreach ($_FILES as $key => $file) {
             if ($file['error'] == UPLOAD_ERR_INI_SIZE) {
-                $errors->add($key, $this->error()
-                );
+                $errors->add($key, $this->error());
             }
         }
 
@@ -29,27 +29,6 @@ class AddMaxFileSizeError
             return $this->response($request, $errors);
 
         return $next($request);
-    }
-
-    private function convert_from_ini_format($size)
-    {
-        $val = trim($size);
-        $last = strtoupper($val[strlen($val) - 1]);
-        switch ($last) {
-            case 'G':
-                $val *= pow(1024, 3);
-                break;
-            case 'M':
-                $val *= pow(1024, 2);
-                break;
-            case 'K':
-                $val *= 1024;
-                break;
-            default:
-                $val = $size;
-        }
-
-        return $val;
     }
 
     /**
@@ -68,13 +47,16 @@ class AddMaxFileSizeError
 
     private function convert_to_human_readable($file_max)
     {
+
+        if ($file_max >= pow(1024, 4))
+            return $this->fun($file_max, 4, 'terabyte');
         if ($file_max >= pow(1024, 3))
-            return ($file_max / pow(1024, 3)) . ' gigabytes';
+            return $this->fun($file_max, 3, 'gigabyte');
         if ($file_max >= pow(1024, 2))
-            return ($file_max / pow(1024, 2)) . ' megabytes';
-        if ($file_max >= 1024)
-            return ($file_max / 1024) . ' kilobytes';
-        return $file_max . ' bytes';
+            return $this->fun($file_max, 2, 'megabyte');
+        if ($file_max >= pow(1024, 1))
+            return $this->fun($file_max, 1, 'kilobyte');
+        return $this->fun($file_max, 0, 'byte');
     }
 
     /**
@@ -83,11 +65,14 @@ class AddMaxFileSizeError
      */
     private function error()
     {
-        $file_max_human_readable = ini_get('upload_max_filesize');
-        $file_max = $this->convert_from_ini_format($file_max_human_readable);
-
         return 'The file may not be greater than ' .
-        $this->convert_to_human_readable($file_max) .
+        $this->convert_to_human_readable(UploadedFile::getMaxFilesize()) .
         '.';
+    }
+
+    private function fun($file_max, $power, $unit)
+    {
+        $count = $file_max / pow(1024, $power);
+        return $count . ' ' . ($count == 1 ? $unit : str_plural($unit));
     }
 }
