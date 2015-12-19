304018f1a1e105b7a5ac294802285f94894b3620
Justin Nesselrotte <jnesselr@harding.edu>
2015-12-18 22:44:07 -0700

    Removing template from job index view

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -3,7 +3,6 @@
 @section('title', str_plural(Auth::user()->name). ' jobs')
 
 @section('content')
-
     <div id="jobs">
         <div class="row">
             <div class="span6">
@@ -22,28 +21,6 @@
             </div>
         </div>
     </div>
-
-    <template id="jobs-template">
-        <h1>@{{ name  }} :: @{{ range }} :: <a :href="allLink">See All</a></h1>
-        <table v-if="count > 0" class="table table-striped table-bordered table-condensed">
-            <thead>
-            <tr>
-                <th>#</th>
-                <th>Name</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="job in jobs">
-                <td>@{{ job.id }}</td>
-                <td>@{{ job.name }}</td>
-            </tr>
-            </tbody>
-        </table>
-        <div v-if="count == 0">
-            No jobs to see here
-        </div>
-    </template>
-
 @stop
 
 @section('end-js')
