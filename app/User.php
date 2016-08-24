55cccf028fdc45804c3a149a590b68e41d522d6a
Justin Nesselrotte <jnesselr@harding.edu>
2016-08-24 01:39:03 -0600

    Migrating user model over

--- app/User.php
+++ app/User.php
@@ -4,10 +4,18 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +34,34 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    public function getRegisteredAttribute()
+    {
+        return $this->created_at;
+    }
+
+    public function getLastSeenAttribute()
+    {
+        return $this->updated_at;
+    }
+
+    public function activities()
+    {
+        return $this->hasMany('App\Models\Activity');
+    }
+
+    public function queues()
+    {
+        return $this->hasMany('App\Models\Queue');
+    }
+
+    public function bots()
+    {
+        return $this->hasMany('App\Models\Bot');
+    }
 }
